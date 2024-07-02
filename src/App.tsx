0f9d33cedfaa37ca9b6ac390f848099870135b75
kizey <ikizey@icloud.com>
2024-07-03 03:04:15 +0400

    feat: implement main application component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,63 @@
 import { Component } from "react";
+import Search from "./components/Search";
+import Results from "./components/Results";
+import TestErrorButton from "./components/TestErrorButton";
+import { fetchStarWarsCharacters } from "./api/starWarsAPI";
+import Loading from "./components/Loading";
+import styles from "./App.module.css";
+
+interface AppState {
+  results: { name: string; gender: string; url: string }[];
+  loading: boolean;
+}
 
 class App extends Component {
+  state: AppState = {
+    results: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.handleFetchResults(localStorage.getItem("searchTerm") || "");
+  }
+
+  handleFetchResults = async (searchTerm: string) => {
+    this.setState({ loading: true });
+    try {
+      const results = await fetchStarWarsCharacters(searchTerm);
+      this.setState({ results, loading: false });
+    } catch (error) {
+      console.error("API Error:", error);
+      this.setState({ loading: false });
+    }
+  };
+
+  handleSearch = (term: string) => {
+    this.handleFetchResults(term);
+  };
+
   render() {
-    return <h1>Hello, class</h1>;
+    return (
+      <div className={styles.appContainer}>
+        <section>
+          <h2 className={styles.sectionHeader}>
+            Search for a Star Wars characters
+          </h2>
+          <Search onSearch={this.handleSearch} />
+        </section>
+        <section>
+          <h2 className={styles.sectionHeader}>Search Results:</h2>
+          {this.state.loading ? (
+            <Loading />
+          ) : (
+            <Results results={this.state.results} />
+          )}
+        </section>
+        <div className={styles.topRight}>
+          <TestErrorButton />
+        </div>
+      </div>
+    );
   }
 }
 
