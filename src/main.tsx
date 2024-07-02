460c58870a057a77ee22659c85f521d838f135d5
kizey <ikizey@icloud.com>
2024-07-02 16:36:21 +0400

    feat: add ErrorBoundary with ErrorFallback to main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import ErrorBoundary from "./components/ErrorBoundary.tsx";
+import ErrorFallback from "./components/ErrorFallback.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={ErrorFallback}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
