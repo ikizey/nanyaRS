c37c6b648c64f9a99e3356161df00ca9b6bac4c5
kizey <ikizey@icloud.com>
2024-07-06 02:28:47 +0400

    feat: add  Root route component

---
+++ src/routes/Root.tsx
@@ -0,0 +1,12 @@
+import App from "../App";
+
+export default function Root() {
+  return (
+    <>
+      <div className="searchBar">
+        <App />
+      </div>
+      <div className="details"></div>
+    </>
+  );
+}
