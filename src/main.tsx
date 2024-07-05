d0b74f84f12883953dce6f8e815a93a7116f2db9
kizey <ikizey@icloud.com>
2024-07-06 02:46:34 +0400

    feat: integrate react router for application routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
 import ErrorFallback from "./components/ErrorFallback.tsx";
+import { router } from "./routes/router.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={ErrorFallback}>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>,
 );
