a01d0eb5c369f0f2c97f8e0e91a28da3035f3610
kizey <ikizey@icloud.com>
2024-07-05 20:32:33 +0400

    refactor: convert TestErrorButton component to functional component

--- src/components/TestErrorButton.tsx
+++ src/components/TestErrorButton.tsx
@@ -1,24 +1,16 @@
-import { Component } from "react";
+import { useState } from "react";
 import styles from "./TestErrorButton.module.css";
 
-class TestErrorButton extends Component {
-  state = { hasError: false };
+export default function TestErrorButton() {
+  const [hasError, setHasError] = useState(false);
 
-  handleClick = () => {
-    this.setState({ hasError: true });
-  };
-
-  render() {
-    if (this.state.hasError) {
-      throw new Error("This is a test error.");
-    }
-
-    return (
-      <button className={styles.errorButton} onClick={this.handleClick}>
-        Throw Test Error
-      </button>
-    );
+  if (hasError) {
+    throw new Error("This is a test error.");
   }
-}
 
-export default TestErrorButton;
+  return (
+    <button className={styles.errorButton} onClick={() => setHasError(true)}>
+      Throw Test Error
+    </button>
+  );
+}
