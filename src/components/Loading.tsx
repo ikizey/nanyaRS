ce65e9ea5dc31ad793973e0185a921a2277d9967
kizey <ikizey@icloud.com>
2024-07-05 20:20:19 +0400

    refactor: convert Loading component to functional component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,14 +1,9 @@
-import { Component } from "react";
 import styles from "./Loading.module.css";
 
-class Loading extends Component {
-  render() {
-    return (
-      <div className={styles.loading}>
-        <p>Loading...</p>
-      </div>
-    );
-  }
+export default function Loading() {
+  return (
+    <div className={styles.loading}>
+      <p>Loading...</p>
+    </div>
+  );
 }
-
-export default Loading;
