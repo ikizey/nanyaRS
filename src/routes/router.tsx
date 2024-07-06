84d0d53ae146e7427356247c3ceb4864607a40c7
kizey <ikizey@icloud.com>
2024-07-06 11:20:33 +0400

    feat: add not found route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
+import NotFound from "./NotFound";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
