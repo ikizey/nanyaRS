29a8a1b21df4b064a8e1816eb3f4a42544df1845
kizey <ikizey@icloud.com>
2024-07-09 15:49:28 +0400

    feat: use react router navigation to return from error screen

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./ErrorFallback.module.css";
 
-export default function ErrorFallback({ onReset }: { onReset: () => void }) {
+export default function ErrorFallback() {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.errorFallback}>
       <p className={styles.errorMessage}>Something went wrong!</p>
       <p className={styles.errorMessage}>We're working on fixing it.</p>
-      <button className={styles.resetButton} onClick={onReset}>
+      <button
+        className={styles.resetButton}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
         Try Again
       </button>
     </div>
