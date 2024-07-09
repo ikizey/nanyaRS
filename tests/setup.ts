0d83488fea21cc1b509b3e2490b76674cf43bd8f
kizey <ikizey@icloud.com>
2024-07-10 01:05:06 +0400

    feat: mock api calls

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,15 @@
-import { expect, afterEach } from "vitest";
+import { expect, afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
+import * as mockStarWarsAPI from "./mocks/starWarsAPI";
 
 expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
 });
+
+vi.mock("../src/api/starWarsAPI", () => ({
+  fetchStarWarsCharacters: mockStarWarsAPI.fetchStarWarsCharacters,
+  fetchStarWarsCharacter: mockStarWarsAPI.fetchStarWarsCharacter,
+}));
