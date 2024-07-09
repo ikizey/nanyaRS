065a989fdf05d8ee9d6f2e38c1d45f22fe252d50
kizey <ikizey@icloud.com>
2024-07-09 15:54:53 +0400

    feat: update hook to novigate to searchterm saved in LS on intial render

--- src/hooks/useSearchTerm.tsx
+++ src/hooks/useSearchTerm.tsx
@@ -1,19 +1,37 @@
 import { useState, useEffect, useCallback } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function useSearchTerm() {
-  const [searchTerm, setSearchTerm] = useState(
-    localStorage.getItem("searchTerm") || "",
-  );
-
-  const saveSearchTerm = useCallback((searchTerm: string) => {
-    localStorage.setItem("searchTerm", searchTerm);
+  const LSSearchKeyName = "searchTermRS";
+  const loadSearchTerm = useCallback(() => {
+    return localStorage.getItem(LSSearchKeyName) || "";
   }, []);
 
+  const saveSearchTerm = useCallback((searchTerm: string) => {
+    localStorage.setItem(LSSearchKeyName, searchTerm);
+  }, []);
+
+  const [searchTerm, setSearchTerm] = useState(loadSearchTerm);
+  const navigate = useNavigate();
+  const pathname = useLocation().pathname;
+  const [firstRender, setFirstRender] = useState(true);
+
   useEffect(() => {
+    if (firstRender) {
+      setFirstRender(false);
+      if (searchTerm) {
+        navigate(`${pathname}?search=${searchTerm}`);
+      }
+    }
     return () => {
       saveSearchTerm(searchTerm);
     };
-  }, [searchTerm, saveSearchTerm]);
+  }, [saveSearchTerm, searchTerm, pathname, navigate, firstRender]);
 
-  return [searchTerm, setSearchTerm, saveSearchTerm] as const;
+  return {
+    searchTerm,
+    setSearchTerm,
+    loadSearchTerm,
+    saveSearchTerm,
+  };
 }
