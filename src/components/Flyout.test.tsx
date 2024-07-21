2e4046237b2e61de20354538ac017d3e19c06d1e
kizey <ikizey@icloud.com>
2024-07-21 13:27:10 +0400

    refactor: flyout tests

--- src/components/Flyout.test.tsx
+++ src/components/Flyout.test.tsx
@@ -1,10 +1,10 @@
+import { MemoryRouter } from "react-router-dom";
 import { describe, it, expect, vi } from "vitest";
-import { setup, screen, waitFor } from "../__tests__/setup";
-import Results from "./Results";
+import { setup, screen } from "../__tests__/setup";
+import { characters } from "../__tests__/mocks/starWarsAPI";
 import { store } from "../store";
 import { unselectAllItems } from "../features/selectedItems/selectedItemsSlice";
-import { characters } from "../__tests__/mocks/starWarsAPI";
-import { MemoryRouter } from "react-router-dom";
+import Results from "./Results";
 
 describe("Flyout", () => {
   beforeEach(() => {
@@ -25,20 +25,18 @@ describe("Flyout", () => {
 
   it("does not render when no items are selected", () => {
     renderResults();
-    const flyout = screen.queryByText(/items? selected/i);
-    expect(flyout).not.toBeInTheDocument();
+
+    expect(screen.queryByText(/items? selected/i)).not.toBeInTheDocument();
   });
 
-  it("renders when an item is selected", async () => {
+  it('renders "1 item selected" when an item is selected', async () => {
     const { user } = renderResults();
 
     const checkbox = screen.getAllByRole("checkbox")[0];
     await user.click(checkbox);
     expect(checkbox).toBeChecked();
 
-    await waitFor(() => {
-      expect(screen.getByText(/1 item selected/i)).toBeInTheDocument();
-    });
+    expect(screen.getByText(/1 item selected/i)).toBeInTheDocument();
   });
 
   it("renders correct number of selected items", async () => {
@@ -49,50 +47,42 @@ describe("Flyout", () => {
     await user.click(firstCheckbox);
     expect(firstCheckbox).toBeChecked();
 
-    await waitFor(() => {
-      expect(screen.getByText(/1 item selected/i)).toBeInTheDocument();
-    });
+    expect(screen.getByText(/1 item selected/i)).toBeInTheDocument();
 
     const secondCheckbox = checkboxes[1];
     await user.click(secondCheckbox);
     expect(secondCheckbox).toBeChecked();
 
-    await waitFor(() => {
-      expect(screen.getByText(/2 items selected/i)).toBeInTheDocument();
-    });
+    expect(screen.getByText(/2 items selected/i)).toBeInTheDocument();
   });
 
-  it("deselects all items when 'Deselect all' button is clicked", async () => {
+  it('deselects all items when "Deselect all" button is clicked', async () => {
     const { user } = renderResults();
 
     const checkboxes = screen.getAllByRole("checkbox");
 
     await user.click(checkboxes[0]);
     await user.click(checkboxes[1]);
-    await waitFor(() => {
-      expect(screen.getByText(/2 items selected/i)).toBeInTheDocument();
-    });
+    expect(screen.getByText(/2 items selected/i)).toBeInTheDocument();
 
-    const deselectAllButton = screen.getByText(/deselect all/i);
+    const deselectAllButton = screen.getByRole("button", {
+      name: /deselect all/i,
+    });
     await user.click(deselectAllButton);
 
-    await waitFor(() => {
-      expect(screen.queryByText(/items selected/i)).not.toBeInTheDocument();
-    });
+    expect(screen.queryByText(/items selected/i)).not.toBeInTheDocument();
   });
 
-  it("downloads the selected items as a CSV file", async () => {
+  it("downloads button points to correct csv file", async () => {
     const { user } = renderResults();
 
     const checkboxes = screen.getAllByRole("checkbox");
 
     await user.click(checkboxes[0]);
     await user.click(checkboxes[1]);
-    await waitFor(() => {
-      expect(screen.getByText(/2 items selected/i)).toBeInTheDocument();
-    });
+    expect(screen.getByText(/2 items selected/i)).toBeInTheDocument();
 
-    const downloadButton = screen.getByText(/download/i);
+    const downloadButton = screen.getByRole("button", { name: /download/i });
 
     expect(downloadButton).toHaveAttribute("href");
     expect(downloadButton).toHaveAttribute("download", "2_characters.csv");
