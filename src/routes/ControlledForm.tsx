97b169ca50fac9510e1e2e4e3051176c2957c127
kizey <ikizey@icloud.com>
2024-08-13 21:04:24 +0400

    feat: add image field

--- src/routes/ControlledForm.tsx
+++ src/routes/ControlledForm.tsx
@@ -1,68 +1,111 @@
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
-import { addFormData, FormInputData } from "../store/formSlice";
+import { addFormData, FormInputData, Gender } from "../store/formSlice";
+import { readFile } from "../lib/readFile";
+import Preview from "../components/Preview";
+
+export interface RawFormInputData {
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  gender: Gender;
+  termsAccepted: boolean;
+  file: File[];
+}
 
 export default function ControlledForm() {
-  const { register, handleSubmit } = useForm<FormInputData>();
+  const { register, handleSubmit } = useForm<RawFormInputData>();
   const dispatch = useDispatch();
 
-  function onSubmit(data: FormInputData) {
-    dispatch(addFormData(data));
+  async function onSubmit(data: RawFormInputData) {
+    let imageBase64: string | undefined;
+    if (data.file) {
+      imageBase64 = await readFile(data.file[0] as File);
+    }
+
+    const finalData: FormInputData = {
+      name: data.name,
+      age: data.age,
+      email: data.email,
+      password: data.password,
+      confirmPassword: data.confirmPassword,
+      gender: data.gender,
+      termsAccepted: data.termsAccepted,
+      imageBase64,
+    };
+
+    dispatch(addFormData(finalData));
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h2>Controlled form</h2>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input id="name" {...register("name")} />
-      </div>
-      <div>
-        <label htmlFor="age">Age:</label>
-        <input
-          id="age"
-          {...register("age", { valueAsNumber: true })}
-          type="number"
-          min="0"
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input id="email" {...register("email")} type="email" />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input id="password" {...register("password")} type="password" />
-      </div>
-      <div>
-        <label htmlFor="confirmPassword">Confirm Password:</label>
-        <input
-          id="confirmPassword"
-          {...register("confirmPassword")}
-          type="password"
-        />
-      </div>
-      <div>
-        <label>Gender:</label>
-        <input id="male" {...register("gender")} type="radio" value="male" />
-        <label htmlFor="male">Male</label>
-        <input
-          id="female"
-          {...register("gender")}
-          type="radio"
-          value="female"
-        />
-        <label htmlFor="female">Female</label>
-      </div>
-      <div>
-        <input
-          id="termsAccepted"
-          {...register("termsAccepted")}
-          type="checkbox"
-        />
-        <label htmlFor="termsAccepted">I accept the Terms and Conditions</label>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2>Controlled form</h2>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input id="name" {...register("name")} />
+        </div>
+        <div>
+          <label htmlFor="age">Age:</label>
+          <input
+            id="age"
+            {...register("age", { valueAsNumber: true })}
+            type="number"
+            min="0"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input id="email" {...register("email")} type="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input id="password" {...register("password")} type="password" />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input
+            id="confirmPassword"
+            {...register("confirmPassword")}
+            type="password"
+          />
+        </div>
+        <div>
+          <label>Gender:</label>
+          <input id="male" {...register("gender")} type="radio" value="male" />
+          <label htmlFor="male">Male</label>
+          <input
+            id="female"
+            {...register("gender")}
+            type="radio"
+            value="female"
+          />
+          <label htmlFor="female">Female</label>
+        </div>
+        <div>
+          <input
+            id="termsAccepted"
+            {...register("termsAccepted")}
+            type="checkbox"
+          />
+          <label htmlFor="termsAccepted">
+            I accept the Terms and Conditions
+          </label>
+        </div>
+        <div>
+          <label htmlFor="file">Upload Picture:</label>
+          <input
+            id="file"
+            type="file"
+            accept="image/png, image/jpeg"
+            {...register("file")}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <Preview />
+    </>
   );
 }
--- src/routes/UnControlledForm.tsx
+++ src/routes/UnControlledForm.tsx
@@ -1,72 +1,112 @@
 import { FormEvent } from "react";
 import { useDispatch } from "react-redux";
-import { addFormData, FormInputData } from "../store/formSlice";
+import { addFormData, FormInputData, Gender } from "../store/formSlice";
+import { readFile } from "../lib/readFile";
+import Preview from "../components/Preview";
 
-export function convertFormData(data: FormData) {
-  const formEntries = Object.fromEntries(data.entries());
+export async function convertFormData(data: FormData) {
+  const rawName = data.get("name") as string;
+  const name = rawName;
+
+  const rawAge = data.get("age") as string;
+  const age = Number(rawAge);
+
+  const rawEmail = data.get("email") as string;
+  const email = rawEmail;
+
+  const rawPassword = data.get("password") as string;
+  const password = rawPassword;
+
+  const rawConfirmPassword = data.get("confirmPassword") as string;
+  const confirmPassword = rawConfirmPassword;
+
+  const rawGender = data.get("gender") as string;
+  const gender = rawGender as Gender;
+
+  const rawTermsAccepted = data.get("termsAccepted") as string;
+  const termsAccepted = rawTermsAccepted === "on";
+
+  const rawFile = data.get("file") as File;
+  const imageBase64 = await readFile(rawFile);
+
   const formData: FormInputData = {
-    name: formEntries.name as string,
-    age: Number(formEntries.age),
-    email: formEntries.email as string,
-    password: formEntries.password as string,
-    confirmPassword: formEntries.confirmPassword as string,
-    gender: formEntries.gender as "male" | "female",
-    termsAccepted: formEntries.termsAccepted === "on",
+    name,
+    age,
+    email,
+    password,
+    confirmPassword,
+    gender,
+    termsAccepted,
+    imageBase64,
   };
   return formData;
 }
 
-export function getFormData(event: FormEvent) {
+export async function getFormData(event: FormEvent) {
   const form = event.currentTarget as HTMLFormElement;
   const formData = new FormData(form);
-  return convertFormData(formData);
+  return await convertFormData(formData);
 }
 
 export default function UnControlledForm() {
   const dispatch = useDispatch();
 
-  function handleSubmit(event: FormEvent) {
+  async function handleSubmit(event: FormEvent) {
     event.preventDefault();
-    const data = getFormData(event);
+    const data = await getFormData(event);
 
     dispatch(addFormData(data));
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Uncontrolled form</h2>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input id="name" name="name" type="text" />
-      </div>
-      <div>
-        <label htmlFor="age">Age:</label>
-        <input id="age" name="age" type="number" min="0" />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input id="email" name="email" type="email" />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input id="password" name="password" type="password" />
-      </div>
-      <div>
-        <label htmlFor="confirmPassword">Confirm Password:</label>
-        <input id="confirmPassword" name="confirmPassword" type="password" />
-      </div>
-      <div>
-        <label>Gender:</label>
-        <input id="male" name="gender" type="radio" value="male" />
-        <label htmlFor="male">Male</label>
-        <input id="female" name="gender" type="radio" value="female" />
-        <label htmlFor="female">Female</label>
-      </div>
-      <div>
-        <input id="termsAccepted" name="termsAccepted" type="checkbox" />
-        <label htmlFor="termsAccepted">I accept the Terms and Conditions</label>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Uncontrolled form</h2>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input id="name" name="name" type="text" />
+        </div>
+        <div>
+          <label htmlFor="age">Age:</label>
+          <input id="age" name="age" type="number" min="0" />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input id="email" name="email" type="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input id="password" name="password" type="password" />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input id="confirmPassword" name="confirmPassword" type="password" />
+        </div>
+        <div>
+          <label>Gender:</label>
+          <input id="male" name="gender" type="radio" value="male" />
+          <label htmlFor="male">Male</label>
+          <input id="female" name="gender" type="radio" value="female" />
+          <label htmlFor="female">Female</label>
+        </div>
+        <div>
+          <input id="termsAccepted" name="termsAccepted" type="checkbox" />
+          <label htmlFor="termsAccepted">
+            I accept the Terms and Conditions
+          </label>
+        </div>
+        <div>
+          <label htmlFor="file">Upload Picture:</label>
+          <input
+            id="file"
+            name="file"
+            type="file"
+            accept="image/png, image/jpeg"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <Preview />
+    </>
   );
 }
