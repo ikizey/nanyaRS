51d486a45121af524529778b2499a4e2d254ef30
kizey <ikizey@icloud.com>
2024-08-13 21:02:41 +0400

    feat: add readFile function

---
+++ src/lib/readFile.ts
@@ -0,0 +1,9 @@
+export function readFile(file: File): Promise<string> {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      resolve(reader.result as string);
+    };
+    reader.readAsDataURL(file);
+  });
+}
