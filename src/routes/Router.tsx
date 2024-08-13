6d6d340fcdba6781d3bfb838100c517eb3a928b5
kizey <ikizey@icloud.com>
2024-08-13 14:11:30 +0400

    feat: add and setup router

---
+++ src/routes/Router.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Index from "./Index";
+import NotFoundPage from "./NotFoundPage";
+import ControlledForm from "./ControlledForm";
+import UnControlledForm from "./UnControlledForm";
+import ErrorPage from "./ErrorPage";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} errorElement={<ErrorPage />}>
+        <Route path="uncontrolled" element={<UnControlledForm />} />
+        <Route path="controlled" element={<ControlledForm />} />
+      </Route>
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+}
