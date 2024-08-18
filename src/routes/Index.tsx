756a56560595a32999b2bbcceaa305bdc21a8c44
kizey <ikizey@icloud.com>
2024-08-18 13:58:41 +0400

    feat: add styling is not a requirement text

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -3,8 +3,12 @@ import Preview from "../components/Preview";
 
 export default function Index() {
   return (
-    <div>
-      <ul>
+    <div style={{ padding: "20px" }}>
+      <h2 style={{ color: "coral", margin: "0 auto" }}>
+        styling is not a requirement
+      </h2>
+      <br />
+      <ul style={{ listStyle: "none" }}>
         <li>
           <Link to="/uncontrolled">uncontrolled form</Link>
         </li>
