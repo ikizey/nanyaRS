49a00313ec638b8d265f229aa06e880a1cfe11e3
kizey <ikizey@icloud.com>
2024-08-18 13:57:16 +0400

    feat: add changes highlighting

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+import HighlightedChange from "./HighlightedChange";
 
 export default function Preview() {
   const data = useSelector((state: RootState) => state.forms.formData);
@@ -11,35 +12,38 @@ export default function Preview() {
       <hr />
       <h2>Submission Preview</h2>
       <div>
-        <p>
-          <strong>Name:</strong> {data.name}
-        </p>
-        <p>
-          <strong>Age:</strong> {data.age}
-        </p>
-        <p>
-          <strong>Email:</strong> {data.email}
-        </p>
-        <p>
-          <strong>Gender:</strong> {data.gender}
-        </p>
-        <p>
-          <strong>Terms Accepted:</strong> {data.termsAccepted ? "Yes" : "No"}
-        </p>
-        <p>
-          <strong>Password:</strong> {data.password}
-        </p>
-        <p>
-          <strong>Confirm Password:</strong> {data.confirmPassword}
-        </p>
-        <p>
-          <strong>Uploaded Image:</strong>
-          <br />
-          <img src={data.imageBase64} alt="" />
-        </p>
-        <p>
-          <strong>Country</strong> {data.country}
-        </p>
+        <HighlightedChange changed={data.name.changed}>
+          <strong>Name:</strong> {data.name.value}
+        </HighlightedChange>
+        <HighlightedChange changed={data.age.changed}>
+          <strong>Age:</strong> {data.age.value}
+        </HighlightedChange>
+        <HighlightedChange changed={data.email.changed}>
+          <strong>Email:</strong> {data.email.value}
+        </HighlightedChange>
+        <HighlightedChange changed={data.gender.changed}>
+          <strong>Gender:</strong> {data.gender.value}
+        </HighlightedChange>
+        <HighlightedChange changed={data.termsAccepted.changed}>
+          <strong>Terms Accepted:</strong>{" "}
+          {data.termsAccepted.value ? "Yes" : "No"}
+        </HighlightedChange>
+        <HighlightedChange changed={data.password.changed}>
+          <strong>Password:</strong> {data.password.value}
+        </HighlightedChange>
+        <HighlightedChange changed={data.confirmPassword.changed}>
+          <strong>Confirm Password:</strong> {data.confirmPassword.value}
+        </HighlightedChange>
+        {data.imageBase64?.value && (
+          <HighlightedChange changed={data.imageBase64.changed}>
+            <strong>Uploaded Image:</strong>
+            <br />
+            <img src={data.imageBase64.value} alt="" />
+          </HighlightedChange>
+        )}
+        <HighlightedChange changed={data.country.changed}>
+          <strong>Country:</strong> {data.country.value}
+        </HighlightedChange>
       </div>
     </div>
   );
--- src/store/formSlice.ts
+++ src/store/formSlice.ts
@@ -14,12 +14,39 @@ export interface FormInputData {
   country: string;
 }
 
+export interface TrackedField<T> {
+  value: T;
+  changed: boolean;
+}
+
+export interface TrackedFormInputData {
+  name: TrackedField<string>;
+  age: TrackedField<number>;
+  email: TrackedField<string>;
+  password: TrackedField<string>;
+  confirmPassword: TrackedField<string>;
+  gender: TrackedField<string>;
+  termsAccepted: TrackedField<boolean>;
+  imageBase64?: TrackedField<string | undefined>;
+  country: TrackedField<string>;
+}
+
 export interface FormState {
-  formData?: FormInputData;
+  formData?: TrackedFormInputData;
 }
 
 const initialState: FormState = {
   formData: undefined,
+};
+
+const trackFormField = <T>(
+  field: TrackedField<T>,
+  newValue: T,
+): TrackedField<T> => {
+  return {
+    value: newValue,
+    changed: field.value !== newValue,
+  };
 };
 
 const formSlice = createSlice({
@@ -27,7 +54,44 @@ const formSlice = createSlice({
   initialState,
   reducers: {
     addFormData(state, action: PayloadAction<FormInputData>) {
-      state.formData = action.payload;
+      const formData: TrackedFormInputData = state.formData || {
+        name: { value: "", changed: false },
+        age: { value: 0, changed: false },
+        email: { value: "", changed: false },
+        password: { value: "", changed: false },
+        confirmPassword: { value: "", changed: false },
+        gender: { value: "", changed: false },
+        termsAccepted: { value: false, changed: false },
+        imageBase64: { value: undefined, changed: false },
+        country: { value: "", changed: false },
+      };
+
+      formData.name = trackFormField(formData.name, action.payload.name);
+      formData.age = trackFormField(formData.age, action.payload.age);
+      formData.email = trackFormField(formData.email, action.payload.email);
+      formData.password = trackFormField(
+        formData.password,
+        action.payload.password,
+      );
+      formData.confirmPassword = trackFormField(
+        formData.confirmPassword,
+        action.payload.confirmPassword,
+      );
+      formData.gender = trackFormField(formData.gender, action.payload.gender);
+      formData.termsAccepted = trackFormField(
+        formData.termsAccepted,
+        action.payload.termsAccepted,
+      );
+      formData.imageBase64 = trackFormField(
+        formData.imageBase64!,
+        action.payload.imageBase64 || undefined,
+      );
+      formData.country = trackFormField(
+        formData.country,
+        action.payload.country,
+      );
+
+      state.formData = formData;
     },
   },
 });
